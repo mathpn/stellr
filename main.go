a7d7b64d42b36df7f3245592a7438c9a00d1b4bc
Matheus Pedroni <pnmatheus@protonmail.com>
2024-07-25 22:15:57 -0300

    clean unused code

--- main.go
+++ main.go
@@ -288,26 +288,6 @@ func (index *trieIndexBuilder) Build() SearchIndex {
 	}
 }
 
-func ReadCorpus(filename string) ([]string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close()
-
-	var lines []string
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, err
-	}
-
-	return lines, nil
-}
-
 type App struct {
 	indexBuilder IndexBuilder
 	index        SearchIndex
